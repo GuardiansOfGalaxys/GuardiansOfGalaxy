26ed8bb45427db3c7165afdfc4cc0f3d248539d9
duongth201 <hoduong201@gmail.com>
2023-03-03 01:45:50 +0700

    Hero: add Joystick, but not success

--- Assets/Scripts/Character/PlayerController.cs
+++ Assets/Scripts/Character/PlayerController.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Character player;
+    //Joystick
+    public FixedJoystick Joystick;
+    Rigidbody2D rb;
+    Vector2 move;
+    public float moveSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
         player = InitPlayer.player;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         player.Update();
+        move.x = Joystick.Horizontal;
+        move.y = Joystick.Vertical;
+        //rotation
+        float hAxis = move.x;
+        float vAxis = move.y;
+        float zAxis = Mathf.Atan2(hAxis, vAxis) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0f, 0f, -zAxis);
     }
+
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
+    }
+
 }
